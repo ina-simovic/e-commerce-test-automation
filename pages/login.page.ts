acf10d8c21066f94008b469baedea25f2869253b
Irina Simovic <irina.simovic.ina@gmail.com>
2025-10-12 21:31:52 +0200

    Refactor LoginPage and enhance login tests with visibility checks for fields and buttons.

--- pages/login.page.ts
+++ pages/login.page.ts
@@ -1,4 +1,4 @@
-import { expect, type Locator, type Page } from '@playwright/test';
+import { type Locator, type Page } from '@playwright/test';
 
 export class LoginPage {
   readonly page: Page;
@@ -13,5 +13,19 @@ export class LoginPage {
     this.usernameField = page.locator('#user-name');
     this.passwordField = page.locator('#password');
     this.loginButton = page.locator('#login-button');
+  }
+
+  async setUsername(username: string): Promise<void> {
+    await this.usernameField.fill(username);
+  }
+
+  async setPassword(password: string): Promise<void> {
+    await this.passwordField.fill(password);
+  }
+
+  async logInWith(options: { username: string; password: string }) {
+    await this.usernameField.fill(options.username);
+    await this.passwordField.fill(options.password);
+    await this.loginButton.click();
   }
 }
--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -1,14 +1,52 @@
 import { test, expect } from '@playwright/test';
 import { LoginPage } from '../pages/login.page';
 
-test('page title "Swag Labs"', async ({ page }) => {
+test.beforeEach(async ({ page }) => {
   await page.goto('/');
+});
+
+test('page title "Swag Labs"', async ({ page }) => {
   await expect(page).toHaveTitle('Swag Labs');
 });
 
 test('header "Swag Labs"', async ({ page }) => {
-  await page.goto('/');
   const loginPage = new LoginPage(page);
   await expect(loginPage.header).toBeVisible();
   await expect(loginPage.header).toHaveText('Swag Labs');
+});
+
+test('username field is displayed correctly', async ({ page }) => {
+  const loginPage = new LoginPage(page);
+  await expect(loginPage.usernameField).toBeVisible();
+  await expect(loginPage.usernameField).toHaveAttribute(
+    'placeholder',
+    'Username',
+  );
+  await expect(loginPage.usernameField).toHaveValue('');
+});
+
+test('password field is displayed correctly', async ({ page }) => {
+  const loginPage = new LoginPage(page);
+  await expect(loginPage.passwordField).toBeVisible();
+  await expect(loginPage.passwordField).toHaveAttribute(
+    'placeholder',
+    'Password',
+  );
+  await expect(loginPage.passwordField).toHaveValue('');
+});
+
+test('login button is displayed correctly', async ({ page }) => {
+  const loginPage = new LoginPage(page);
+  await expect(loginPage.loginButton).toBeVisible();
+  await expect(loginPage.loginButton).toHaveAttribute('value', 'Login');
+  await expect(loginPage.loginButton).not.toHaveAttribute('disabled');
+});
+
+test('valid login', async ({ page }) => {
+  const loginPage = new LoginPage(page);
+  await loginPage.logInWith({
+    username: 'standard_user',
+    password: 'secret_sauce',
+  });
+  await expect(page.locator('[data-test="title"]')).toHaveText('Products');
 });
